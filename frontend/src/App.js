127911a0276370b041d31c5bb1f097f7fbd71358
SNarboni <sebastien.narboni@hotmail.fr>
2021-03-30 12:18:16 +0200

    ajout des pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from "./views/Home/Home"
+import AddProduct from "./views/AddProduct/AddProduct"
+import AllProductsPage from "./views/AllProductsPage/AllProductsPage"
+import Login from "./views/Login/Login"
+import Message from "./views/Message/Message"
+import ProductPage from "./views/ProductPage/ProductPage"
+import ProfilePage from "./views/ProfilePage/ProfilePage"
+import Signup from "./views/Signup/Signup"
 
 function App() {
   return (
@@ -8,8 +16,13 @@ function App() {
     <Navbar/>
     <Switch>
       <Route exact path='/' component={Home}/>
-      <Route/>
-      <Route/>
+      <Route path='/AddProduct' component={AddProduct}/>
+      <Route path='/AllProductsPage' component={AllProductsPage}/>
+      <Route path='/Login' component={Login}/>
+      <Route path='/Message' component={Message}/>
+      <Route path='/ProductPage' component={ProductPage}/>
+      <Route path='/ProfilePage' component={ProfilePage}/>
+      <Route path='/Signup' component={Signup}/>
     </Switch>
     </BrowserRouter>
   );
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './Navbar.css'
-
 
 const Navbar = () => {
 
     return (
-        <>
-
-        </>
+        <nav>
+            <Link to="/AddProduct">AddProduct</Link>
+            <Link to="/AllProductsPage">AllProductsPage</Link>
+            <Link to="/Login">Login</Link>
+            <Link to="/Message">Message</Link>
+            <Link to="/ProductPage">ProductPage</Link>
+            <Link to="/ProfilePage">ProfilePage</Link>
+            <Link to="/Signup">Signup</Link>
+        </nav>
     )
 }
 
