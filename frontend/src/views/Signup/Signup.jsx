8b59ab95634681e794f15c6d7f87f7f8b4d57fe7
Mosindo <mosindo.mbilinyama@hotmail.fr>
2021-03-30 17:36:45 +0200

    first commit

--- frontend/src/views/Signup/Signup.jsx
+++ frontend/src/views/Signup/Signup.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from 'react';
+import {useForm} from 'react-hook-form';
+import './Signup.css';
 
 const Signup = () => {
+    const {register,handleSubmit,errors, formState}=useForm();
+    const onSubmit =data=>console.log(data);
+    const {isSubmitting}=formState;
+    console.log(errors)
     return (
-        <div>
-            <h1>Signup</h1>
-        </div>
+        <form id="login-box" onSubmit={handleSubmit(onSubmit)}>
+  <div className="left">
+    <h1>Sign up</h1>
+    
+    <input type="text" name="username" ref={register({required:true})}placeholder="Username" />
+    {errors.username && <p className="alert">This field is required</p>}
+    <input type="text" name="email" ref={register({required:true})}placeholder="E-mail" />
+    {errors.email && <p className="alert">This field is required</p>}
+    <input type="password" name="password" ref={register({required:true})}placeholder="Password" />
+    <input type="password" name="password2" ref={register({required:true})}placeholder="Retype password" />
+    {errors.password && <p className="alert">This field is required</p>}
+    <input type="submit" disabled={isSubmitting} name="signup_submit" value="Sign me up" />
+  </div>
+  
+  {/* <div className="right">
+    <span className="loginwith">Sign in with<br />social network</span>
+    
+    <button className="social-signin facebook">Log in with facebook</button>
+    <button className="social-signin twitter">Log in with Twitter</button>
+    <button className="social-signin google">Log in with Google+</button>
+  </div> */}
+  {/* <div className="or">OR</div> */}
+</form>
     )
 }
 
