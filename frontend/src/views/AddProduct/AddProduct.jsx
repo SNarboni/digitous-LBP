a247cc1cc877d89446ccbca6748167f760a8a65d
SNarboni <sebastien.narboni@hotmail.fr>
2021-03-31 15:41:09 +0200

    début de addProduct

--- frontend/src/views/AddProduct/AddProduct.jsx
+++ frontend/src/views/AddProduct/AddProduct.jsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
-const AddProduct = () => {
-    return (
-        <div>
-            <h1>AddProduct</h1>
-        </div>
-    )
+
+const wait = function (duration = 1000){
+    return new Promise((resolve) => {
+        window.setTimeout(resolve, duration)
+    })
 }
 
-export default AddProduct
+const AddProduct = () => {
+  const { register, handleSubmit, formState, errors } = useForm({
+      mode: 'onTouched'
+  });
+    const {isSubmitting, isSubmitted,isSubmitSuccessful} = formState
+
+  const onSubmit = async (data) => {
+    await wait(2000)
+  };
+
+  console.log(errors)
+
+  return (
+    <form className="container py-5" onSubmit={handleSubmit(onSubmit)}>
+      <h1>Ajouter un produit</h1>
+      {isSubmitSuccessful && <div className="alert alert-success">Merci pour votre inscription</div>}
+      <div className="row">
+        <div className="col-md-6 form-group">
+          <label htmlFor="username">username</label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            name="username"
+            ref={register({required : "vous devez entrer un nom d'utilisateur"})}
+          />
+            {errors.username && <span>{errors.username.message}</span>}
+        </div>
+        <div className="col-md-6 form-group">
+          <label htmlFor="email">email</label>
+          <input
+            type="text"
+            className="form-control"
+            id="email"
+            name="email"
+            ref={register({required : true})}
+          />
+        </div>
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          name="password"
+          ref={register({required : true})}
+        />
+      </div>
+      <button disabled={isSubmitting} className="btn btn-primary">Ajouter</button>
+    </form>
+  );
+};
+
+export default AddProduct;
