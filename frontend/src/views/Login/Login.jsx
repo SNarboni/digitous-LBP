27f020a91162eb13d7ea2be3af069949089b47fe
Mosindo <mosindo.mbilinyama@hotmail.fr>
2021-03-31 11:23:53 +0200

    mon login et signup

--- frontend/src/views/Login/Login.jsx
+++ frontend/src/views/Login/Login.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from 'react';
+import {useForm} from 'react-hook-form';
+import './Login.css';
+
 
 const Login = () => {
+    const { register, errors, handleSubmit, formState } = useForm({});
+    
+        const onSubmit =data=>console.log(data);
+        const {isSubmitting}=formState;
+        console.log(onSubmit)
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
-}
+    <form id="login-box" onSubmit={e => e.preventDefault()}>
+      <div className="left">
+        <h1>Login</h1>
+        <input 
+            type="text" 
+            name="email" ref={register({
+            required:true,pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+            message: "Enter a valid e-mail address",
+        }})}
+        placeholder="E-mail" 
+        />
+    
+        {errors.email && <p className="alert">{errors.email.message}</p>}
+        <input
+            name="password"
+            type="password"
+            ref={register({
+              required: "You must specify a password",
+              minLength: {
+                value: 8,
+                message: "Password must have at least 8 characters"
+              }
+            })}
+            placeholder="Password"
+          />
+          {errors.password && <p>{errors.password.message}</p>}
+    
+         
+        <input type="submit" disabled={isSubmitting} name="signup_submit" value="Sign me up" onClick={handleSubmit(onSubmit)} />
+      </div>
+      
+
+    </form>
+        )
+    }
+    
+
 
 export default Login
--- frontend/src/views/Signup/Signup.jsx
+++ frontend/src/views/Signup/Signup.jsx
@@ -1,25 +1,80 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {useForm} from 'react-hook-form';
 import './Signup.css';
 
 const Signup = () => {
-    const {register,handleSubmit,errors, formState}=useForm();
-    const onSubmit =data=>console.log(data);
+    const { register, errors, handleSubmit, watch, formState } = useForm({});
+    const password = useRef({});
+    password.current = watch("password", "");
+
+    const onSubmit =async (data)=>{
+        await console.log(JSON.stringify(data));
+        // try {
+            
+        //     delete data.password_repeat;
+        //     console.log('Received values of form: ', data);
+        //     const response = await fetch('place du server backend', {
+        //       method: 'POST',
+        //       headers: {
+        //         'content-type': 'application/json'
+        //       },
+        //       body: JSON.stringify(data)
+        //     });
+        //     if (response.ok) {
+        //       const tokenObj = await response.json();
+        //       localStorage.setItem('token', tokenObj.token);
+        //       history.push('/admin');
+        //     }
+        //   } catch (err) {
+        //     console.error(err)
+        // }
+    };
     const {isSubmitting}=formState;
-    console.log(errors)
+    // console.log(onSubmit)
     return (
-        <form id="login-box" onSubmit={handleSubmit(onSubmit)}>
+        <form id="login-box" onSubmit={e => e.preventDefault()}>
   <div className="left">
     <h1>Sign up</h1>
     
     <input type="text" name="username" ref={register({required:true})}placeholder="Username" />
     {errors.username && <p className="alert">This field is required</p>}
-    <input type="text" name="email" ref={register({required:true})}placeholder="E-mail" />
-    {errors.email && <p className="alert">This field is required</p>}
-    <input type="password" name="password" ref={register({required:true})}placeholder="Password" />
-    <input type="password" name="password2" ref={register({required:true})}placeholder="Retype password" />
-    {errors.password && <p className="alert">This field is required</p>}
-    <input type="submit" disabled={isSubmitting} name="signup_submit" value="Sign me up" />
+    <input 
+        type="text" 
+        name="email" ref={register({
+        required:true,pattern: {
+        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+        message: "Enter a valid e-mail address",
+    }})}
+    placeholder="E-mail" 
+    />
+
+    {errors.email && <p className="alert">{errors.email.message}</p>}
+    <input
+        name="password"
+        type="password"
+        ref={register({
+          required: "You must specify a password",
+          minLength: {
+            value: 8,
+            message: "Password must have at least 8 characters"
+          }
+        })}
+        placeholder="Password"
+      />
+      {errors.password && <p>{errors.password.message}</p>}
+
+      <input
+        name="password_repeat"
+        type="password"
+        ref={register({
+          validate: value =>
+            value === password.current || "The passwords do not match"
+        })}
+        placeholder="Repeat password "
+      />
+      {errors.password_repeat && <p>{errors.password_repeat.message}</p>}
+
+    <input type="submit" disabled={isSubmitting} name="signup_submit" value="Sign me up" onClick={handleSubmit(onSubmit)} />
   </div>
   
   {/* <div className="right">
